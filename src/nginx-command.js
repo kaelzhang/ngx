95fa9abde256e02a241e0abbc1eda1521e49b772
Kael Zhang <i+github@kael.me>
2017-06-30 17:49:30 +0800

    1.0.2: commonjs -> es6 modules, fixes cli

--- src/nginx-command.js
+++ src/nginx-command.js
@@ -1,22 +1,30 @@
 import path from 'path'
 
+export default {
+  reload,
+  stop,
+  test,
+  start
+}
+
+
 // @param {path} dest
 function nginx (dest, entry, rest = []) {
   return ['nginx', ['-p', dest, '-c', path.join(dest, entry), ...rest]]
 }
 
-export function reload (dest, entry) {
+function reload (dest, entry) {
   return nginx(dest, entry, ['-s', 'reload'])
 }
 
-export function stop (dest, entry) {
+function stop (dest, entry) {
   return nginx(dest, entry, ['-s', 'stop'])
 }
 
-export function test (dest, entry) {
+function test (dest, entry) {
   return nginx(dest, entry, ['-t'])
 }
 
-export function start (dest, entry) {
+function start (dest, entry) {
   return nginx(dest, entry)
 }
